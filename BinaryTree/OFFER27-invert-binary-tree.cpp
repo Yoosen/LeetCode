2c13b6865c1a1fa13965c73f6b2a34f91c23e10a
Yoosen <1255254248@qq.com>
2022-03-30 21:36:29 +0800

    剑指 Offer 27 & LeetCode 226  二叉树的镜像

---
+++ BinaryTree/OFFER27-invert-binary-tree.cpp
@@ -0,0 +1,52 @@
+// 剑指 Offer 27 & LeetCode 226  二叉树的镜像 
+// 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+
+// 例如输入：
+
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+// 镜像输出：
+
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
+
+ 
+
+// 示例 1：
+
+// 输入：root = [4,2,7,1,3,6,9]
+// 输出：[4,7,2,9,6,3,1]
+ 
+
+// 限制：
+
+// 0 <= 节点个数 <= 1000
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if(root == NULL)
+    		return NULL;
+
+    	TreeNode* temp = root->left;
+    	root->left = mirrorTree(root->right);
+    	root->right = mirrorTree(temp);
+
+        return root;
+    }
+};
