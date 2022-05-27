77db4d2b71cd83aaa1392ca31481521c3928740b
Yoosen <1255254248@qq.com>
2022-05-27 15:04:09 +0800

    LeetCode 152 乘积最大子数组

---
+++ DynamicProgramming/152-maximum-product-subarray.cpp
@@ -0,0 +1,48 @@
+// LeetCode 152 乘积最大子数组
+
+// 152. 乘积最大子数组
+// 给你一个整数数组 nums ，请你找出数组中乘积最大的非空连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+
+// 测试用例的答案是一个 32-位 整数。
+
+// 子数组 是数组的连续子序列。
+
+ 
+
+// 示例 1:
+
+// 输入: nums = [2,3,-2,4]
+// 输出: 6
+// 解释: 子数组 [2,3] 有最大乘积 6。
+// 示例 2:
+
+// 输入: nums = [-2,0,-1]
+// 输出: 0
+// 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ 
+
+// 提示:
+
+// 1 <= nums.length <= 2 * 104
+// -10 <= nums[i] <= 10
+// nums 的任何前缀或后缀的乘积都 保证 是一个 32-位 整数
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+    	int n = nums.size(), imax = 1, imin = 1, res = INT_MIN;
+
+    	for(int i = 0; i < n; i++) {
+    		if(nums[i] < 0) {
+    			int tmp = imax;
+    			imax = imin;
+    			imin = tmp;
+    		}
+    		imax = max(imax * nums[i], nums[i]);
+    		imin = min(imin * nums[i], nums[i]);
+
+    		res = max(res, imax);	// 保存结果
+    	}
+    	return res;
+    }
+};
