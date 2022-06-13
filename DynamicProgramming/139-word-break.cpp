6855b35817421dc71885228480ceb36039147a8d
Yoosen <1255254248@qq.com>
2022-06-13 10:55:11 +0800

    139. 单词拆分

---
+++ DynamicProgramming/139-word-break.cpp
@@ -0,0 +1,57 @@
+// 139. 单词拆分
+// 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。请你判断是否可以利用字典中出现的单词拼接出 s 。
+
+// 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+
+ 
+
+// 示例 1：
+
+// 输入: s = "leetcode", wordDict = ["leet", "code"]
+// 输出: true
+// 解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+// 示例 2：
+
+// 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+// 输出: true
+// 解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+//      注意，你可以重复使用字典中的单词。
+// 示例 3：
+
+// 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+// 输出: false
+ 
+
+// 提示：
+
+// 1 <= s.length <= 300
+// 1 <= wordDict.length <= 1000
+// 1 <= wordDict[i].length <= 20
+// s 和 wordDict[i] 仅有小写英文字母组成
+// wordDict 中的所有字符串 互不相同
+
+// 若dp[i] = True且s[i,… ,j)在wordlist中: dp[j] = True。
+// 解释: 
+// dp[i] = True 说明s的前i位可以用wordDict表示，则s[i,…,j)出现在wordDict中，
+// 说明 s 的前 j 位可以表示。
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+    	vector<bool> dp(s.size() + 1, false);
+    	dp[0] = true;
+
+    	for(int i = 0; i < s.size(); ++i) {
+    		// 当前不能匹配，直接跳过
+    		if(!dp[i])
+    			continue;
+
+    		for(auto& word : wordDict) {
+    			if(i + word.size() <= s.size() && s.substr(i, word.size()) == word)
+    				dp[i + word.size()] = true;
+    		}
+    	}
+
+    	return dp[s.size()];
+    }
+};
